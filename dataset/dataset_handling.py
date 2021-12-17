6b7cbea790da77ecdf5875f66584af2ebee4e2bf
Margoo9 <39960184+Margoo9@users.noreply.github.com>
2021-12-17 16:48:30 +0100

    Add images handling functions

---
+++ dataset/dataset_handling.py
@@ -0,0 +1,37 @@
+import cv2
+import numpy as np
+import os
+
+
+def load_image(path_to_image):
+    image = cv2.imread(path_to_image)
+    return image
+
+
+# normalize data in range [-1, 1]
+def normalize_image(image):
+    # image = cv2.reshape(image, (256, 256), interpolation = cv2.INTER_AREA)
+    image = cv2.resize(image, (256, 256))
+    image = np.array(image)
+    image = (image - 127.5) / 127.5
+    return image
+
+
+def normalize_with_numpy(image):
+    normalized_image = (image - np.amin(image)) / (np.amax(image) - np.amin(image))
+    normalized_image = 2 * normalized_image - 1
+    return normalized_image
+
+
+def deprocess_image(image):
+    image = image * 127.5 + 127.5
+    image = image.astype('uint8')
+    return image
+
+
+def save_image(image, path):
+    image = image * 127.5 + 127.5
+    cv2.imwrite(os.path.join(path, 'result.jpg'), image)
+
+
+
