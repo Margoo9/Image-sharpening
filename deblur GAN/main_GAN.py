224d60c1d9948098c05abba5007532db23c1ee42
Margoo9 <39960184+Margoo9@users.noreply.github.com>
2021-12-18 23:13:44 +0100

    Add test function

--- deblur GAN/main_GAN.py
+++ deblur GAN/main_GAN.py
@@ -68,5 +68,25 @@ def train(batch_size, epoch_num, discriminator_train_num=5):
         d.save_weights(os.path.join(WEIGHTS_DIR, 'discriminator_{}.h5'.format(epoch)), True)
 
         
+def test(batch_size):
+    data = load_data('./dataset/Test', batch_size)
+    y_test, x_test = data['sharp'], data['blur']
+    g = generator_model()
+    g.load_weights('weights/generator.h5')
+    generated_images = g.predict(x=x_test, batch_size=batch_size)
+    generated = np.array([deprocess_image(img) for img in generated_images])
+    x_test = deprocess_image(x_test)
+    y_test = deprocess_image(y_test)
+
+    for i in range(generated_images.shape[0]):
+        y = y_test[i, :, :, :]
+        x = x_test[i, :, :, :]
+        img = generated[i, :, :, :]
+        output = np.concatenate((y, x, img), axis=1)
+        im = Image.fromarray(output.astype(np.uint8))
+        im.save('res{}.jpg'.format(i)) 
+        
+        
 if __name__ == '__main__':
     train(16, 4)    
+    test(4)
