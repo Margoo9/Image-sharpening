2bcc0dc61dd00a281b3671ee0d114ee1ed0cbbcd
Margoo9 <39960184+Margoo9@users.noreply.github.com>
2021-12-21 19:17:13 +0100

    Add all networks

--- network_testing/network_test.py
+++ network_testing/network_test.py
@@ -1,36 +1,88 @@
-import os
 import numpy as np
 from PIL import Image
-from dataset.dataset_handling import normalize_image, deprocess_image, load_image
-import tensorflow.keras.models
+import os
+import cv2
+import glob as gb
+from tensorflow.python import keras as K
+import matplotlib.pyplot as plt
+
+from dataset_handling import load_image, deprocess_image, normalize_image, load_images_100
+from deblurGAN.model import generator_model
 
 
-# model - chosen network (GAN, DBSRCNN or our own)
-def test_network(model, weight_path, input_dir, output_dir):
-    model.load_weights(weight_path)
-    for image_name in os.listdir(input_dir):
-        image = np.array([normalize_image(load_image(os.path.join(input_dir, image_name)))])
-        x_test = image
-        generated_images = model.predict(x=x_test)
-        generated = np.array([deprocess_image(img) for img in generated_images])
-        x_test = deprocess_image(x_test)
-        for i in range(generated_images.shape[0]):
-            x = x_test[i, :, :, :]
-            img = generated[i, :, :, :]
-            output = np.concatenate((x, img), axis=1)
-            im = Image.fromarray(output.astype(np.uint8))
-            im.save(os.path.join(output_dir, image_name))
+# is_size_100
+# True if deblur neural network is used
+# False if DBSRCNN or deblur GAN
+is_size_100 = True
+
+# is_GAN
+# True if GAN is tested
+# False if other networks
+is_GAN = False
+
+path_to_test_image = './images_before_test'
+path_to_model = './model.h5'
+path_to_model_weights = './model_weights.h5'
+path_to_result_save = './images_after_test/'
+batch_size = 4
 
 
-test_before_path = os.path.join('images_before_test')
-test_before_path_with_image = os.path.join('images_before_test', 'testowa.jpg')
-test_after_path = os.path.join('images_after_test')
-test_weights_path = os.path.join('model1.h5')
-test_model_path = os.path.join('./model.h5')
-# prepare image (load and normalize)
-test_image = load_image(test_before_path_with_image)
-test_image = normalize_image(test_image)
-# load model
-test_model = tensorflow.keras.models.load_model(test_model_path)
-# load weights, launch the network, deprocess and save image all in one
-test_network(test_model, test_weights_path, test_before_path, test_after_path)
+def network_test(model, weights, test_image, out_dir, batch_size):
+    if is_size_100:
+        test_img = gb.glob(test_image)
+        x_test = load_images_100(test_img)
+
+        chosen_model = K.models.load_model(model)
+        chosen_model.load_weights(weights)
+
+        res = chosen_model.predict(x_test)
+        res_0 = res[0] * 255.0
+        res_0 = res_0.reshape(100, 100)
+        # plt.imshow(res_0, cmap='gray')
+        # plt.imshow(preds_0)
+        # plt.show()
+        # cv2.imwrite('result.png', res_0)
+        im = Image.fromarray(res_0.astype(np.uint8))
+        im.save(os.path.join(out_dir, 'result.png'))
+    elif is_GAN:
+        images_path = gb.glob(test_image)
+        x_test = []
+        for image_path in images_path:
+            image_blur = load_image(image_path)
+            x_test.append(np.array(image_blur))
+
+        x_test = np.array(x_test).astype(np.float32)
+        x_test = normalize_image(x_test)
+
+        g = generator_model()
+        g.load_weights(weights)
+
+        generated_img = g.predict(x=x_test, batch_size=batch_size)
+        generated_img = deprocess_image(generated_img)
+        for i in range(generated_img.shape[0]):
+            image_generated = generated_img[i, :, :, :]
+            im = Image.fromarray(image_generated)
+            im.save(os.path.join(out_dir, 'result.png'))
+    else:
+        images_path = gb.glob(test_image)
+        x_test = []
+        for image_path in images_path:
+            image_blur = load_image(image_path)
+            x_test.append(np.array(image_blur))
+
+        x_test = np.array(x_test).astype(np.float32)
+        x_test = normalize_image(x_test)
+
+        chosen_model = K.models.load_model(model)
+        chosen_model.load_weights(weights)
+
+        generated_img = chosen_model.predict(x=x_test, batch_size=batch_size)
+        generated_img = deprocess_image(generated_img)
+        for i in range(generated_img.shape[0]):
+            image_generated = generated_img[i, :, :, :]
+            im = Image.fromarray(image_generated)
+            im.save(os.path.join(out_dir, 'result.png'))
+
+
+network_test(path_to_model, path_to_model_weights, path_to_test_image, path_to_result_save, batch_size)
+
